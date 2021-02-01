2f66371c21e4b6b1b7d4f2df6ce8e79ec27996c7
Al Abotsi <albert.abotsi@hotmail.com>
2021-02-01 00:12:27 -0600

    First commit.

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+           // passwordField(),
+           // submitButton(),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      )
+    );
+  }
+
+  Widget passwordField() {
+
+  }
+
+  Widget submitButton() {
+
+  }
+}
